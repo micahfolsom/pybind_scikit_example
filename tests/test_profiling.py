58b0db52edb17388508d48fb413ac3d8b6bc0fc3
Micah Folsom <micahfolsom@gmail.com>
2022-06-17 17:33:09 -0700

    Add Nones

--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -3,33 +3,33 @@
 import pybind_scikit_example as pbske
 
 
-def test_init():
+def test_init() -> None:
     pbske.profile_init(50, 50)
 
 
-def test_1d_numpy():
+def test_1d_numpy() -> None:
     pbske.profile_1d_numpy(50, 50)
 
 
-def test_1d_vector():
+def test_1d_vector() -> None:
     pbske.profile_1d_vector(50, 50)
 
 
-def test_2d_numpy():
+def test_2d_numpy() -> None:
     pbske.profile_2d_numpy(50, 50)
 
 
-def test_3d_numpy_copy():
+def test_3d_numpy_copy() -> None:
     pbske.profile_3d_numpy_copy(50, 50)
 
 
-def test_3d_numpy_colmaj():
+def test_3d_numpy_colmaj() -> None:
     pbske.profile_3d_numpy_colmaj(50, 50)
 
 
-def test_3d_numpy_memcpy():
+def test_3d_numpy_memcpy() -> None:
     pbske.profile_3d_numpy_memcpy(50, 50)
 
 
-def test_all():
+def test_all() -> None:
     pbske.profile_all(50, 50)
